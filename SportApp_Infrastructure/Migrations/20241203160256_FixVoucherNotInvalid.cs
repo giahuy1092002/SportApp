4d499e0a4f059571f56456f812969b799d4f0ece
giahuy2002 <78332589+giahuy2002@users.noreply.github.com>
2024-12-03 23:06:09 +0700

    Add voucher to sport field

---
+++ SportApp_Infrastructure/Migrations/20241203160256_FixVoucherNotInvalid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportApp_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixVoucherNotInvalid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "VoucherId",
+                table: "Booking",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VoucherId",
+                table: "Booking");
+        }
+    }
+}
