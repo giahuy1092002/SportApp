b7e2a495e48944d84d62d793585ec9587b7e2757
giahuy2002 <78332589+giahuy2002@users.noreply.github.com>
2024-12-08 01:03:23 +0700

    Add is right rejected for booking

---
+++ SportApp_Infrastructure/Migrations/20241207180223_Add-BookingIsRight.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportApp_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingIsRight : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRight",
+                table: "Booking",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reason",
+                table: "Booking",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRight",
+                table: "Booking");
+
+            migrationBuilder.DropColumn(
+                name: "Reason",
+                table: "Booking");
+        }
+    }
+}
